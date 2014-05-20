1947ed34364633a88b517415a682d962dd1bfacb
ryelle <kelly.dwan@10up.com>
2014-05-19 20:09:09 -0400

    Correct path for customizer JS

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'museum_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function museum_customize_preview_js() {
-	wp_enqueue_script( 'museum_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'museum_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'museum_customize_preview_js' );
