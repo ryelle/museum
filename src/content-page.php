456e4263e5c8b87762a80239e3d64c98580b32cf
ryelle <kelly.dwan@gmail.com>
2015-01-16 13:44:25 -0500

    Add featured images to pages

--- src/content-page.php
+++ src/content-page.php
@@ -6,6 +6,12 @@
  */
 ?>
 
+<?php if ( has_post_thumbnail() ): ?>
+<div class="entry-image">
+	<?php the_post_thumbnail( 'single' ); ?>
+</div><!-- .entry-image -->
+<?php endif; ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
