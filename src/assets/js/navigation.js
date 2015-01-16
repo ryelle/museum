3d77a41bef59dd6e6c13f9d322e8bd391018bba0
ryelle <kelly.dwan@gmail.com>
2015-01-16 15:59:38 -0500

    Pull in `fixMenuTouchTaps()` from wp.com, fixes 46

--- src/assets/js/navigation.js
+++ src/assets/js/navigation.js
@@ -4,36 +4,81 @@
  * Handles toggling the navigation menu for small screens.
  */
 ( function( $ ) {
+	var navigationWrapper,
+	    linksList = [];
+
+	// Fix child menus for touch devices.
+	function fixMenuTouchTaps( linksList ) {
+		var touchStartFn;
+
+		if ( 'ontouchstart' in window ) {
+			touchStartFn = function( e ) {
+				var menuItem = this.parentNode;
+				if ( ! menuItem.classList.contains( 'focus' ) ) {
+					e.preventDefault();
+					for( var i = 0; i < menuItem.parentNode.children.length; ++i ) {
+						if ( menuItem === menuItem.parentNode.children[i] ) {
+							continue;
+						}
+						menuItem.parentNode.children[i].classList.remove( 'focus' );
+					}
+					menuItem.classList.add( 'focus' );
+				} else {
+					menuItem.classList.remove( 'focus' );
+				}
+			};
+			for ( var i = 0; i < linksList.length; ++i ) {
+				linksList[i].addEventListener( 'touchstart', touchStartFn, false )
+			}
+		}
+	}
+
+	navigationWrapper = document.getElementById( 'site-navigation' );
+	linksList = navigationWrapper.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+	fixMenuTouchTaps( linksList );
+
+	postsWrapper = document.getElementById( 'main' );
+	if ( postsWrapper ) {
+		linksList = postsWrapper.querySelectorAll( '.with-image > a' );
+		fixMenuTouchTaps( linksList );
+	}
+
 	var container, button, menu;
-
 	container = document.getElementById( 'site-navigation' );
-	if ( ! container )
+	if ( ! container ) {
 		return;
-
+	}
 	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button )
+	if ( 'undefined' === typeof button ) {
 		return;
-
+	}
 	menu = container.getElementsByTagName( 'ul' )[0];
-
 	// Hide menu toggle button if menu is empty and return early.
 	if ( 'undefined' === typeof menu ) {
 		button.style.display = 'none';
 		return;
 	}
 
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+	menu.setAttribute( 'aria-expanded', 'false' );
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
 		menu.className += ' nav-menu';
+	}
 
 	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) )
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
 			container.className = container.className.replace( ' toggled', '' );
-		else
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+		} else {
 			container.className += ' toggled';
+			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
 	};
 
-	// make dropdowns functional on focus
+	// make dropdowns functional on focus (keyboard)
 	$( '.nav-wrapper' ).find( 'a' ).on( 'focus blur', function() {
-		$( this ).parents( 'li' ).toggleClass( 'focus' );
+		$( this ).parents('li').toggleClass( 'focus' );
 	} );
+
 } )( jQuery );
