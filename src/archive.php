beb667a179d635614955f903946caa84867c5a84
ryelle <kelly.dwan@gmail.com>
2015-03-18 17:28:19 -0400

    Add 4.1 functions `the_archive_title` and `the_archive_description`— except for Author pages, because we have a special display for author info

--- src/archive.php
+++ src/archive.php
@@ -15,70 +15,18 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( '<span class="author-image">%s</span><span class="vcard">%s</span>', get_avatar( get_the_author_meta( 'ID' ), 128 ), get_the_author() );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'museum' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'museum' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'museum' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'museum' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'museum' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'museum' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'museum' );
-
-						else :
-							_e( 'Archives', 'museum' );
-
-						endif;
-					?>
-				</h1>
 				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
+					if ( is_author() ) {
+						printf( '<h1 class="page-title"><span class="author-image">%s</span><span class="vcard">%s</span></h1>', get_avatar( get_the_author_meta( 'ID' ), 128 ), get_the_author() );
 
-					$bio = get_the_author_meta( 'description', get_the_author_meta( 'ID' ) );
-					if ( ! empty( $bio ) && is_author() ):
-						printf( '<div class="author-description">%s</div>', apply_filters( 'the_content', $bio ) );
-					endif;
+						$bio = get_the_author_meta( 'description', get_the_author_meta( 'ID' ) );
+						if ( ! empty( $bio ) ) {
+							printf( '<div class="author-description">%s</div>', apply_filters( 'the_content', $bio ) );
+						}
+					} else {
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					}
 				?>
 			</header><!-- .page-header -->
 
