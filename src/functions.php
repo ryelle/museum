c0f980c6b6c84c6ac910242ff856e6d53384e1da
ryelle <kelly.dwan@gmail.com>
2015-03-19 12:58:26 -0400

    Update version number

--- src/functions.php
+++ src/functions.php
@@ -102,7 +102,7 @@ add_action( 'widgets_init', 'museum_widgets_init' );
 function museum_scripts() {
 	wp_enqueue_style( 'museum-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'museum-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array( 'jquery' ), '20120206', true );
+	wp_enqueue_script( 'museum-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array( 'jquery' ), '20150319', true );
 
 	wp_enqueue_script( 'museum-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
